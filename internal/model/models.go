bf66b83d34834d7dd8b085a4310780ce68857111
gablue316-rw <gablue316@gmail.com>
2026-07-06 09:14:42 -0400

    Add GameDateTime to GameDoc
    Add GameDateTime to GameDoc

--- internal/model/models.go
+++ internal/model/models.go
@@ -110,26 +110,27 @@ type GameDescriptor struct {
 }
 
 type GameDoc struct {
-	GameId      int64  `bson:"gameId,omitempty"`
-	Date        string `bson:"date,omitempty"`
-	Time        string `bson:"time,omitempty"`
-	Sport       string `bson:"sport,omitempty"`
-	Site        string `bson:"site,omitempty"`
-	Field       string `bson:"field,omitempty"`
-	NumOfGames  int64  `bson:"numOfGames,omitempty"`
-	Level       string `bson:"level,omitempty"`
-	GameFee     int64  `bson:"gameFee,omitempty"`
-	TravelPay   int64  `bson:"travelPay,omitempty"`
-	AssignorFee int64  `bson:"assignorFee,omitempty"`
-	Deductions  int64  `bson:"deductions,omitempty"`
-	Association string `bson:"association,omitempty"`
-	Status      string `bson:"status,omitempty"`
-	Referee     string `bson:"referee,omitempty"`
-	U1          string `bson:"u1,omitempty"`
-	U2          string `bson:"u2,omitempty"`
-	ECO         string `bson:"eco,omitempty"`
-	Assignor    string `bson:"assignor,omitempty"`
-	TenantId    string `bson:"tenantId"`
+	GameId       int64     `bson:"gameId,omitempty"`
+	Date         string    `bson:"date,omitempty"`
+	Time         string    `bson:"time,omitempty"`
+	GameDateTime time.Time `bson:"gameDateTime,omitempty"`
+	Sport        string    `bson:"sport,omitempty"`
+	Site         string    `bson:"site,omitempty"`
+	Field        string    `bson:"field,omitempty"`
+	NumOfGames   int64     `bson:"numOfGames,omitempty"`
+	Level        string    `bson:"level,omitempty"`
+	GameFee      int64     `bson:"gameFee,omitempty"`
+	TravelPay    int64     `bson:"travelPay,omitempty"`
+	AssignorFee  int64     `bson:"assignorFee,omitempty"`
+	Deductions   int64     `bson:"deductions,omitempty"`
+	Association  string    `bson:"association,omitempty"`
+	Status       string    `bson:"status,omitempty"`
+	Referee      string    `bson:"referee,omitempty"`
+	U1           string    `bson:"u1,omitempty"`
+	U2           string    `bson:"u2,omitempty"`
+	ECO          string    `bson:"eco,omitempty"`
+	Assignor     string    `bson:"assignor,omitempty"`
+	TenantId     string    `bson:"tenantId"`
 }
 
 type JsonDoc struct {
